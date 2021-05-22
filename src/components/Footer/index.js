575e1b8d7707da3d41bf6e8777851065e1eb45a2
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-05-22 17:54:23 +0900

    Add footer animation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Flex, Text } from "@chakra-ui/react";
+import useSlideFadeOnView from '../../hooks/useSlideFadeOnView';
+import { slideFade } from '../../animations';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
+
+    const [ref, animation] = useSlideFadeOnView();
+
     return (
-        <Flex
-            fontSize='18px'
-            direction='column'
-            maxW={{ xs: 'full', lg: '650', xl: '670' }} pb={{ xs: '60px', xxl: '85px' }}
-            align='center'>
-            <Text align='center'>Designed & built by Stepan Pavlov. 2021</Text>
-        </Flex>
+        <motion.div
+            ref={ref}
+            variants={slideFade}
+            initial={{ opacity: 0 }}
+            animate={animation}>
+            <Flex
+                fontSize='18px'
+                direction='column'
+                maxW={{ xs: 'full', lg: '650', xl: '670' }} pb={{ xs: '60px', xxl: '85px' }}
+                align='center'>
+                <Text align='center'>Designed & built by Stepan Pavlov. 2021</Text>
+            </Flex>
+        </motion.div>
     );
 };
 
