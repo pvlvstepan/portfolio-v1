4e0eb5b586181fa242ebb96e35c9bc2079e58b11
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-11 21:30:51 +0900

    Add project

--- src/data/projects/index.js
+++ src/data/projects/index.js
@@ -8,6 +8,14 @@ const projectsData = [
         techStack: ['React', 'Node', 'Chart.JS', 'Leaflet', 'Chakra UI', 'Covid-19 API'],
         liveView: 'https://covid-corona-tracker.netlify.app/',
         github: 'https://github.com/pvlvstepan/covid-19-tracker'
+    },
+    {
+        title: 'Instagram Post Generator - Dentis LLC',
+        img: 'https://place-hold.it/670x377',
+        description: 'This web application was developed for Dentis LLC to ease Instagram post creation. Previously employees had to insert text into Photoshop templates but this app allows to do so in a few clicks. Canvas is used to draw images and texts allowing users to download pictures from the website directly.',
+        techStack: ['React', 'Node', 'Chakra UI', 'Canvas'],
+        liveView: 'https://dentis.netlify.app/',
+        github: 'https://github.com/pvlvstepan/dentis-img-generator'
     }
 ];
 
