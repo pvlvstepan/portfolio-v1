27858f60cc6919de5644850c336741ed80faaeb4
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-16 22:29:02 +0900

    Add MovieLister project

--- src/data/projects/index.js
+++ src/data/projects/index.js
@@ -1,13 +1,22 @@
 import covidTracker from './img/covidTracker.png';
+import movieLister from './img/movieLister.png';
 
 const projectsData = [
     {
         title: 'Covid-19 Tracker',
         img: covidTracker,
         description: 'COVID-19 Tracker web application features various data and statistics represented in interactive charts and a choropleth map with search functionality and data filtering options.',
-        techStack: ['React', 'Node', 'Chart.JS', 'Leaflet', 'Chakra UI', 'Covid-19 API'],
+        techStack: ['React', 'Node', 'Chart.JS', 'Leaflet', 'Chakra UI', 'Covid-19 API', 'Axios'],
         liveView: 'https://covid-corona-tracker.netlify.app/',
         github: 'https://github.com/pvlvstepan/covid-19-tracker'
+    },
+    {
+        title: 'MovieLister',
+        img: movieLister,
+        description: "MovieLister is a single page application that I've developed during a coding interview at Bigdata MSU. The app features movies that are being pulled from YTS.mx API and displays them in a neatly designed pages on the website.",
+        techStack: ['React', 'Axios', 'Node', 'Chakra UI', 'React Router'],
+        liveView: 'https://react-movie-lister.netlify.app/',
+        github: 'https://github.com/pvlvstepan/movie-lister'
     },
     {
         title: 'Instagram Post Generator - Dentis LLC',
